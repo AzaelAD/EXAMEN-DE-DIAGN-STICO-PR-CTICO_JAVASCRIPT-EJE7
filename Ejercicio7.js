84c38ed821c6d4d21d15e87c15bd88621c61a339
Azaral <alazamdel@gmail.com>
2024-08-28 19:11:42 -0700

    Ejercicio 7. Concatenación de arreglos

---
+++ Ejercicio7.js
@@ -0,0 +1,8 @@
+// 7. Concatenación de arreglos
+function concatenarArreglos(arreglo1, arreglo2) {
+    return arreglo1.concat(arreglo2);
+}
+
+const arreglo1 = ["Que", "Tal"];
+const arreglo2 = ["!", "¿Cómo", "estás", "Hoy?"];
+console.log("Arreglo concatenado:", concatenarArreglos(arreglo1, arreglo2));
